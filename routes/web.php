277100017e6575c23020007e93508a78fd43dca3
agungdewantr <69377282+agungdewantr@users.noreply.github.com>
2021-03-02 23:16:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,6 @@ Route::get('/', function () {
     return view('home');
 });
 Route::get('/penjualan', [C_penjualan::class, 'index']);
-// Route::get('/barang', [App\Http\Controllers\C_barang::class, 'index_barang']);
-// Route::get('/tambahbarang', [App\Http\Controllers\C_barang::class, 'tambah_barang']);
-// Route::post('/tambahbarang/proses', [App\Http\Controllers\C_barang::class, 'store_barang']);
-// Route::get('/ubahbarang/{id}',[App\Http\Controllers\C_barang::class, 'ubah_barang']);
 Route::get('/barang/{id}/edit', [App\Http\Controllers\C_barang::class, 'edit']);
 Route::get('/pembelian', [C_pembelian::class, 'index']);
 Route::resource('barang', C_barang::class);
