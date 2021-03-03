0988ff34fb7fa9213656055eecb53f3e957c57d5
agungdewantr <69377282+agungdewantr@users.noreply.github.com>
2021-03-03 10:45:38 +0700

    hmm

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -31,12 +31,18 @@
                             <td>Rp{{$T->Harga}}</td>
                             <td>{{$T->Diskon}}</td>
                             <td>{{$T->Kadaluarsa}}</td>
-                            <td><a href="{{ route('barang.edit',$T->id) }}"><i class="far fa-edit"></i></a>&nbsp;&nbsp;
-                            <form action="{{ route('barang.destroy',$T->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type = "submit"><i class="fas fa-trash-alt" style="color: red"></i></button>
-                            </form>
+                            <td>
+                                <div class="row">
+                                    <div class="col">
+                                        <form action="{{ route('barang.destroy',$T->id) }}" method="POST">
+                                            <a href="{{ route('barang.edit',$T->id) }}"><i class="far fa-edit"></i></a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="button1" type="submit" style="border: none; background-color:rgba(255, 0, 0, 0); position: absolute; width:0.1px; outline:none;"><i class="d-inline fas fa-trash-alt" style="color: red;"></i></button>
+                                        </form>
+                                    </div>
+
+
                             </td>
                         </tr>
                     </tbody>
