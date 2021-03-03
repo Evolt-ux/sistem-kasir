02f88d30ff0d752144d78f3758dffc737f50b94b
Ifan Rendi <55195196+Evolt-ux@users.noreply.github.com>
2021-03-03 13:06:23 +0700

    web eror

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
     return view('home');
 });
 Route::get('/penjualan', [C_penjualan::class, 'index']);
-Route::resource('/pembelian', [C_pembelian::class, 'index']);
+Route::resource('pembelian', C_pembelian::class);
 Route::resource('barang', C_barang::class);
